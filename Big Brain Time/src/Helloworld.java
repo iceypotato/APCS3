181d9c09a27f0476eddba198a8dd99046c5ec2f3
APCS3rd Nicky <APCS3rd@MHS-LT9-12>
2019-08-20 10:12:14 -0700

    Complete hello world project.

---
+++ Big Brain Time/src/Helloworld.java
@@ -0,0 +1,10 @@
+//Nicholas Lee
+//8/16/2019
+//This will print Hello world in the most simple way possible for APCS 3rd period.
+
+public class Helloworld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ Big Brain Time/src/Helloworld2.java
@@ -0,0 +1,50 @@
+//Nicholas Lee
+//8/16/2019
+//This will print Hello world in the most complicated way possible for APCS 3rd period.
+
+public class Helloworld2 {
+	
+	static char H = 'H';
+	static char e = 'e';
+	static char l = 'l';
+	static char o = 'o';
+	static char w = 'W';
+	static char r = 'r';
+	static char d = 'd';
+	static char excl = '!';
+
+	public static void main(String[] args) {
+		System.out.println(h()+e()+l()+l()+o()+" "+w()+o()+r()+l()+d()+excl);
+	}
+	
+	static String h() {
+		String h = H+"";
+		return h;
+	}
+	
+	static String e() {
+		String ee = e+"";
+		return ee;
+	}
+	
+	static String l() {
+		String ll = l+"";
+		return ll;
+	}
+	static String o() {
+		String oo = o+"";
+		return oo;
+	}
+	static String w() {
+		String ww = w+"";
+		return ww;
+	}
+	static String r() {
+		String rr = r+"";
+		return rr;
+	}
+	static String d() {
+		String dd = d+"";
+		return dd;
+	}
+}
