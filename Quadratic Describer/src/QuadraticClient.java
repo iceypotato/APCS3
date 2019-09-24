1107e53ac26c90ed3d184f57c9f0ac91ecbc0959
Nicholas Lee <niceawlego@gmail.com>
2019-09-24 11:05:49 -0700

    quadratic client

--- Quadratic Describer/src/QuadraticClient.java
+++ Quadratic Describer/src/QuadraticClient.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class QuadraticClient {
 
+	static Scanner scan = new Scanner(System.in);
+	static String exception = "";
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to the Quadratic Describer");
+		System.out.print("a: "); 
+		try {
+			double a = scan.nextDouble();
+		} catch (Exception e) {
+			System.out.println("The value you entered was not a double.");
+			exception = e.getMessage();
+		}
 	}
 
 }
