a31093fd6e3d116c8a1142d2a3b356682de6cad5
Nicholas Lee <niceawlego@gmail.com>
2019-09-26 10:31:48 -0700

    Quad Describer

--- Quadratic Describer/src/Quadratic.java
+++ Quadratic Describer/src/Quadratic.java
@@ -3,14 +3,15 @@
 public class Quadratic {
 	//list the stretch factor, if graph is flipped, and roots
 	public static String quadrDescriber(double a, double b, double c) {
-		System.out.println(a+"x^2" + b+"x" + c+"x");
+		System.out.println(a+"x^2 + " + b+"x + " + c+"x");
 		if (a < 0) {
 			System.out.println("the Graph is inverted.\n" + "Slope: " + a);
 		}
 		System.out.println("Slope: " + a);
 		System.out.println("Discriminant: " + Calculate.discriminant(a, b, c));
+		System.out.println("Y-intercept: " + c);
 		System.out.println(Calculate.quadForm(a, b, c));
 		
-		return "";
+		return "Done!";
 	}
 }
--- Quadratic Describer/src/QuadraticClient.java
+++ Quadratic Describer/src/QuadraticClient.java
@@ -4,15 +4,17 @@ public class QuadraticClient {
 
 	static Scanner scan = new Scanner(System.in);
 	static String exception = "";
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Quadratic Describer");
 		System.out.print("a: "); 
-		try {
-			double a = scan.nextDouble();
-		} catch (Exception e) {
-			System.out.println("The value you entered was not a double.");
-			exception = e.getMessage();
-		}
+		double a = scan.nextDouble();
+		System.out.print("b: ");
+		double b = scan.nextDouble();
+		System.out.print("c: ");
+		double c = scan.nextDouble();
+		System.out.println();
+		Quadratic.quadrDescriber(a, b, c);
 	}
 
 }
