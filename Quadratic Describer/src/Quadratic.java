51031b5712efc603bd1537ca62d43d0c6b231546
Nicky <niceawlego@gmail.com>
2019-09-30 21:37:16 -0700

    Updated Quadratic client and the calculate library

--- Quadratic Describer/src/Quadratic.java
+++ Quadratic Describer/src/Quadratic.java
@@ -3,15 +3,19 @@
 public class Quadratic {
 	//list the stretch factor, if graph is flipped, and roots
 	public static String quadrDescriber(double a, double b, double c) {
-		System.out.println(a+"x^2 + " + b+"x + " + c+"x");
+		String abc = a+"x^2 + " + b+"x + " + c+"x";
+		String inverted = "";
 		if (a < 0) {
-			System.out.println("the Graph is inverted.\n" + "Slope: " + a);
+			inverted = "The graph opens downward.";
 		}
-		System.out.println("Slope: " + a);
-		System.out.println("Discriminant: " + Calculate.discriminant(a, b, c));
-		System.out.println("Y-intercept: " + c);
-		System.out.println(Calculate.quadForm(a, b, c));
+		else {
+			inverted = "The graph opens up.";
+		}
+		String slope = "Stretch Factor: " + a;
+		String discriminant = "Discriminant: " + Calculate.discriminant(a, b, c);
+		String yIntercept = "Y-intercept: (0.0 , " + c + ")";
+		String quadForm = Calculate.quadForm(a, b, c);
 		
-		return "Done!";
+		return "Description of " + abc + "\n\n" + inverted + "\n" + slope + "\n" + discriminant + "\n" + yIntercept + "\n" + quadForm;
 	}
 }
--- Quadratic Describer/src/QuadraticClient.java
+++ Quadratic Describer/src/QuadraticClient.java
@@ -1,3 +1,8 @@
+//Nicholas Lee
+//APCS 3rd
+//September 30, 2019
+//This will describe a quadratic equation from the a, b, and c values.
+
 import java.util.Scanner;
 
 public class QuadraticClient {
@@ -7,6 +12,7 @@ public class QuadraticClient {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Quadratic Describer");
+		System.out.println("Provide values for the variables shown below.");
 		System.out.print("a: "); 
 		double a = scan.nextDouble();
 		System.out.print("b: ");
@@ -14,7 +20,7 @@ public class QuadraticClient {
 		System.out.print("c: ");
 		double c = scan.nextDouble();
 		System.out.println();
-		Quadratic.quadrDescriber(a, b, c);
+		System.out.println(Quadratic.quadrDescriber(a, b, c));
 	}
 
 }
