58a382a19f6d33080b35ce2078f6c1830dbdf4c5
Nicholas Lee <niceawlego@gmail.com>
2019-10-03 10:25:31 -0700

    Fxing processing numebrs

--- Misc Project/src/ProcessingNumbers.java
+++ Misc Project/src/ProcessingNumbers.java
@@ -15,20 +15,15 @@ public class ProcessingNumbers {
 		int input = scanner.nextInt();
 		int max = input;
 		int min = input;
+		int maxEven = input;
 		int sum = 0;
 		String inputString = "";
-		for (int i = 1; i <= numTimes; i++) {
-			if (i == numTimes) {
-				System.out.println("Would you like to go again? (y/N)");
-				inputString = scanner.nextLine();
-				if (inputString.equalsIgnoreCase("y")) {
-					i = 1;
-					System.out.println("How many numbers do you have?");
-					numTimes = scanner.nextInt();
-				}
-			}
+		for (int i = 0; i < numTimes-1; i++) {
 			if (input % 2 == 0) {
 				sum += input;
+			}
+			if (input % 2 == 0 > ) {
+				
 			}
 			if (input > max) {
 				max = input;
@@ -39,18 +34,15 @@ public class ProcessingNumbers {
 			System.out.println("Enter next number.");
 			input = scanner.nextInt();
 		}
-		if (input % 2 == 0) {
-			sum += input;
-		}
-		if (input > max) {
-			max = input;
-		}
-		else if (input < min) {
-			min = input;
-		}
 		System.out.println("Max: " + max);
 		System.out.println("Min: " + min);
-		System.out.println("Sum of even numbers: " + sum);
+		if (sum == 0) {
+			System.out.println("There are no even numbers.");
+		}
+		else {
+			System.out.println("Sum of even numbers: " + sum);
+		}
+
 	}
 
 }
