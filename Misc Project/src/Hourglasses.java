abc31f66ef202321047f915567fbd0e74bef7a8b
Nicholas Lee <niceawlego@gmail.com>
2019-10-08 11:13:59 -0700

    hours glass

--- Misc Project/src/Hourglasses.java
+++ Misc Project/src/Hourglasses.java
@@ -4,22 +4,55 @@ public class Hourglasses {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("how big you want hourglass?");
+		System.out.print("how big you want hourglass? ");
 		int size = scanner.nextInt();
+		System.out.print("|");
 		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size - 2; j++) {
-				for (int a = 0; a < size ; a++) {
-					String quotes = "";
-					
-					for (int ab = 0; ab<size; ab++) {
-						quotes += "\"";
-					}
-					String lineone = "|" + quotes + "|";
-					System.out.println(lineone);
-					
-				}
-			}
+			System.out.print("\"");
 		}
+		System.out.println("|");
+		for (int i = 1; i <= size/2; i++) {
+			if (i == size/2) {
+			}
+			else {
+				System.out.print(printSpace(i));
+				System.out.print("\\");
+				System.out.print(printColons(size-2*i));
+				System.out.println("/");
+			}
+			
+		}
+		System.out.println(printSpace(size/2) + "||");
+		for (int i = size/2;  i >= 0; i--) {
+			if (i <= 1) {
+			}
+			else {
+				System.out.print(printSpace(i-1));
+				System.out.print("/");
+				System.out.print(printColons(2+2*(size/2-i)));
+				System.out.println("\\");
+			}	
+		}
+		System.out.print("|");
+		for (int i = 0; i < size; i++) {
+			System.out.print("\"");
+		}
+		System.out.println("|");
+	}
+	
+	public static String printSpace(int spaces) {
+		String spString = "";
+		for (int i = 0; i < spaces; i++) {
+			spString = spString + " ";
+		}
+		return spString;
+	}
+	public static String printColons(int colons) {
+		String colon = "";
+		for (int i = 0; i < colons; i++) {
+			colon = colon + ":";
+		}
+		return colon;
 	}
 
 }
