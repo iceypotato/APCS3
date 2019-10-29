e05ab3e62e3fb0b92d10e2a0b2c1f06f2f382dcf
Nicky <niceawlego@gmail.com>
2019-10-29 11:07:10 -0700

    arrays lab 3

--- Misc Project/src/ArraysLab3.java
+++ Misc Project/src/ArraysLab3.java
@@ -1,10 +1,24 @@
+import java.util.Arrays;
 
-	/*
+/*
 	 * Create a class named ArraysLab3 that contains 6 static methods (main plus the
 	 * 5 methods defined below).
 	 */
 
 public class ArraysLab3 {
+	
+	public static void main(String[] args) {
+		int[] a1 = {5, 10, 15, 20, 25, 30, 35, 40};
+		int[] a2 = {7, 14, 21, 28, 35, 42, 49, 56};
+		int[] sumArr = sum(a1, a2);
+		int appendNum = 200;
+		int[] appendArr = append(a1, appendNum);
+		System.out.println();
+		System.out.println(Arrays.toString(appendArr));
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
 
 	/*
 	 * 1) Write a method sum that accepts two arrays of integers arr1 and arr2 and
@@ -13,11 +27,14 @@ public class ArraysLab3 {
 	 * arr1 and arr2 have at least one element each and are the same length.
 	 * 
 	 * public static int[] sum(int[] arr1, int[] arr2) {
+	 * 
 	 */
 	public static int[] sum(int[] arr1, int[] arr2) {
-		int[] sumArray = new int[arr1.length];
-		return arr2;
-			
+		int[] arraySum = new int[8];
+		for (int i = 0; i<arr1.length; i++) {	
+			arraySum[i] = arr1[i] + arr2[i];
+		}
+		return arraySum;
 	}
 	
 	/* 
@@ -30,7 +47,9 @@ public class ArraysLab3 {
 	 */
 	
 	public static int[] append(int[] arr, int num) {
-		return null;
+		int[] appended = new int[arr.length+1];
+		appended[appended.length-1] = num;
+		return appended;
 			
 	}
 		
@@ -41,16 +60,22 @@ public class ArraysLab3 {
 	 * arr.length – 1). You can assume arr has at least two elements.
 	 * 
 	 * public static int[] remove(int[] arr, int idx) {
-	 * 
-	 * 4) Write a method sumEven that accepts an array of integers arr and returns
+	 */
+	
+	public static int[] remove(int[] arr, int idx) {
+		return arr;
+		
+	}
+	 
+	/* 4) Write a method sumEven that accepts an array of integers arr and returns
 	 * an integer containing the sum of the elements at the even indices of arr.
 	 * (That means elements at indices 0,2,4,6,8.) You can assume arr has at least
 	 * one element.
 	 * 
 	 * public static int sumEven(int[] arr) {
-	 * 
-	 * 
-	 * 5) Write a method rotateRight that accepts an array of integers arr and does
+	 */ 
+	
+	/* 5) Write a method rotateRight that accepts an array of integers arr and does
 	 * not return a value. The rotateRight method moves each element of arr one
 	 * index to the right (element 0 goes to element 1, element 1 goes to element 2,
 	 * …, element n-1 goes to element 0). You can assume arr has at least one
@@ -59,9 +84,4 @@ public class ArraysLab3 {
 	 * public static void rotateRight(int[] arr) {
 	 * 
 	 */
-
-	public static void main(String[] args) {
-
-	}
-
 }
