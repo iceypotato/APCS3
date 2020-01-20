48cf005231912849e7a76d2c457fbd1e444cb7fb
iceypotato <niceawlego@gmail.com>
2020-01-19 18:15:20 -0800

    gitignore

--- Misc Project/src/TestFraction.java
+++ Misc Project/src/TestFraction.java
@@ -3,10 +3,13 @@ import fracCalc.Fraction;
 public class TestFraction {
 
 	public static void main(String[] args) {
-		Fraction frac = new Fraction("5");
-		Fraction frac2 = new Fraction("-6");
-		frac.multiply(frac2);
-		System.out.println(frac.toString());
+		Fraction frac1 = new Fraction("2");
+		Fraction frac2 = new Fraction("3");
+		System.out.println(frac1);
+		System.out.println(frac2);
+		frac1.add(frac2);
+		System.out.println(frac1);
+		
 	}
 
 }
