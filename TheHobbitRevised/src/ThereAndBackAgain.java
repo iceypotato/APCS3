71cd36891f102398790f3e3f1950ec7524c2456b
Nicky <niceawlego@gmail.com>
2020-02-11 11:38:06 -0800

    Pushed

--- TheHobbitRevised/src/ThereAndBackAgain.java
+++ TheHobbitRevised/src/ThereAndBackAgain.java
@@ -14,25 +14,13 @@ public class ThereAndBackAgain
 		// Create a traveling party called party1 by creating an array of Travelers 
 		// and filling it with frodo, sam, and gimli
 		Traveler[] party1 = {frodo,sam,gimli};
-<<<<<<< HEAD
 		
 		// Then, use a loop to make all travelers go a distance of 50 miles
-=======
-
-		// Then, use a loop to make all travelers go a distance of 50 miles  
-		// Then, for each Traveler in the travelingParty, print their name and how far they've
-		//    traveled in miles.  (In the nextpiece , you'll do this in methods, but 
-		//    for a first pass, just do it in main and print to the console.)
-		// Expected output:  Frodo has traveled 50 miles.
-		//                   Sam has traveled 50 miles.
-		//                   Gimli has traveled 50 miles.
->>>>>>> branch 'master' of https://github.com/iceypotato/APCS3.git
 		for (int i = 0; i < party1.length; i++) {
 			party1[i].travel(50);
 			System.out.println(party1[i].getName()+" has traveled " + party1[i].getDistanceTraveled()+ " miles.");
 		}
 		
-<<<<<<< HEAD
 		// Then, for each Traveler in the travelingParty, print their name and how far they've
 		//    traveled in miles.  (In the next piece, you'll do this in methods, but 
 		//    for a first pass, just do it in main and print to the console.)
@@ -41,8 +29,6 @@ public class ThereAndBackAgain
 		//                   Gimli has traveled 50 miles.		
 		System.out.println();
 		
-=======
->>>>>>> branch 'master' of https://github.com/iceypotato/APCS3.git
 		System.out.println("\n\n\nPART 2: \n");
 
 		String[] dwarfNames = {"Fili", "Kili", "Dori", "Ori", "Nori", "Balin", "Dwalin",
@@ -54,7 +40,6 @@ public class ThereAndBackAgain
 		// Make a new Hobbit called "Bilbo" and add him to party2
 		Hobbit bilbo = new Hobbit("Bilbo");
 		party2.add(bilbo);
-<<<<<<< HEAD
 
 		// Make a new Wizard called "Gandalf" and add him to party2.
 		Wizard gandalf = new Wizard("Gandalf", "Gray");
@@ -71,36 +56,6 @@ public class ThereAndBackAgain
 
 		//Make sure your code prints out the name and distances party2 has traveled.
 		allTravel(party2, 100);
-=======
-		// Make a new Wizard called "Gandalf" and add him to party2.
-		Wizard gandalf = new Wizard("Gandalf", "blue");
-		party2.add(gandalf);
-
-		// write createParty
-		// Call the createParty method and pass it party2 and the dwarfNames array.
-		// create party should add all the new dwarves to party2,
-		createParty(party2, dwarfNames);
-		
-
-		// Write allTravel
-		// Finally, call the allTravel method passing it party2 and 100 (representing
-		// the 100 miles that party2 has traveled together.
-		allTravel(party2, 100);
-
-		// Make sure your code prints out the name and distances party2 has traveled.
-	}
-	public static void createParty(ArrayList<Traveler> party, String[] names) {
-		for (String eachName : names) {
-			Traveler traveler = new Traveler(eachName);
-			party.add(traveler);
-		}
-	}
-	public static void allTravel(ArrayList<Traveler> party, int miles) {
-		for (Traveler eachTraveler : party) {
-			eachTraveler.travel(miles);
-			System.out.println(eachTraveler.getName() + " has traveled " + eachTraveler.getDistanceTraveled() + " miles!");
-		}
->>>>>>> branch 'master' of https://github.com/iceypotato/APCS3.git
 	}
 	
 	public static void createParty(ArrayList<Traveler> party, String[] dwarfNames) {
